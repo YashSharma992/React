024d474ff9f05bc666d6b432ac902e100ae479fb
Yash Sharma <syash6183@gmail.com>
2026-04-21 15:24:56 +0530

    Experiment-4

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,98 +8,18 @@ import Student from './components/Student'
 import React, { useEffect } from "react";
 import "./App.css";
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-
-  const [users, setUsers] = useState([]);
-  const [apiData, setApiData] = useState([]);
-
-  // --------------------------
-  // Fetch API Data using useEffect
-  // --------------------------
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setApiData(data))
-      .catch(() => setApiData([]));
-  }, []);
-
-  // --------------------------
-  // Form Submit Handler
-  // --------------------------
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setError("");
-    setSuccess("");
-
-    if (!name || !email || !password) {
-      setError("All fields are required.");
-      return;
-    }
-
-    if (!email.includes("@")) {
-      setError("Enter a valid email.");
-      return;
-    }
-
-    const newUser = { name, email };
-    setUsers([...users, newUser]);
-    setSuccess("Registration Successful!");
-
-    setName("");
-    setEmail("");
-    setPassword("");
-  };
-
   return (
-    <div className="container">
-      <h2>Registration Form</h2>
+    <>
+      <div className="app">
+      <h1>Student Information</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <input
-          type="email"
-          placeholder="Enter Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <input
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button type="submit">Register</button>
-      </form>
-
-      {error && <p className="error">{error}</p>}
-      {success && <p className="success">{success}</p>}
-
-      {/* Registered Users */}
-      <div className="userBox">
-        <h3>Registered Users</h3>
-        <ul>
-          {users.map((u, index) => (
-            <li key={index}>
-              {u.name} - {u.email}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Student name="Yash Sharma" course="Computer Science and Engineering" marks={97} />
+      <Student name="Anita Verma" course="Information Technology" marks={92} />
+      <Student name="Rohan Gupta" course="Electronics" marks={78} />
+      
     </div>
-  );
+    </>
+  )
 }
 
 export default App
